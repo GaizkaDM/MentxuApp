4d6b8e710dfb840c40e3d9c710b6bbc7a123479b
GaizkaDM <gaizkadm2@gmail.com>
2025-12-10 09:28:40 +0100

    FEAT Agregados puntos de interes en el mapa

--- app/src/main/java/com/gaizkafrost/mentxuapp/MapaActivity.kt
+++ app/src/main/java/com/gaizkafrost/mentxuapp/MapaActivity.kt
@@ -26,9 +26,23 @@ class MapaActivity : AppCompatActivity(),OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Código de ejemplo para verificar que funciona
-        val santurtzi = LatLng(43.330, -3.033)
-        mMap.addMarker(MarkerOptions().position(santurtzi).title("Marcador en Santurtzi"))
+        // Lista de lugares para añadir marcadores
+        val locations = listOf(
+            "Itsas-museoa" to LatLng(43.330639, -3.030750),
+            "Itsas-portua" to LatLng(43.330417, -3.030722),
+            "Agurtza itsasontzia" to LatLng(43.327000, -3.023778),
+            "Santurtziko Udala (Mentxu)" to LatLng(43.328833, -3.032944),
+            "“El niño y el perro” eskultura" to LatLng(43.328833, -3.032306),
+            "“Monumento niños y niñas de la guerra” eskultura" to LatLng(43.330500, -3.029917)
+        )
+
+        // Añadir los marcadores al mapa
+        locations.forEach { (title, latLng) ->
+            mMap.addMarker(MarkerOptions().position(latLng).title(title))
+        }
+
+        // Centrar la cámara en un punto intermedio de Santurtzi y ajustar el zoom
+        val santurtzi = LatLng(43.329, -3.029)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(santurtzi, 15f))
     }
 }
