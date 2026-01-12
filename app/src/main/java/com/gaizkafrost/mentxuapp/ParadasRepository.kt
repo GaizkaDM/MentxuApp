c3579588b15623aae06d485bca94bec6889ccf24
GaizkaDM <gaizkadm2@gmail.com>
2026-01-12 08:13:29 +0100

    CHORE: Reordenadas paradas 1 y 4

--- app/src/main/java/com/gaizkafrost/mentxuapp/ParadasRepository.kt
+++ app/src/main/java/com/gaizkafrost/mentxuapp/ParadasRepository.kt
@@ -6,10 +6,10 @@ object ParadasRepository {
 
     // Lista privada de paradas
     private val paradas = mutableListOf(
-        Parada(1, "Itsas-museoa", LatLng(43.330639, -3.030750)),
+        Parada(1, "Santurtziko Udala (Mentxu)", LatLng(43.328833, -3.032944)),
         Parada(2, "Itsas-portua", LatLng(43.330417, -3.030722)),
         Parada(3, "Agurtza itsasontzia", LatLng(43.327000, -3.023778)),
-        Parada(4, "Santurtziko Udala (Mentxu)", LatLng(43.328833, -3.032944)),
+        Parada(4, "Itsas-museoa", LatLng(43.330639, -3.030750)),
         Parada(5, "“El niño y el perro” eskultura", LatLng(43.328833, -3.032306)),
         Parada(6, "“Monumento niños y niñas de la guerra” eskultura", LatLng(43.330500, -3.029917))
     )
