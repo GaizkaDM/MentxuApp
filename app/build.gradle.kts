0ee1cfb01a1dc0f9d8ba08e4fe1efdd5e3f4b0bb
diegofg11 <diiegofernandezg@gmail.com>
2026-02-12 21:42:11 +0100

    DOCS: Añadido Dokka para la documentación a partir de JavaDocs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dokka)
     id("com.google.devtools.ksp") version "2.0.0-1.0.21"
 }
 
